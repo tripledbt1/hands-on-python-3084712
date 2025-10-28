7e7bf4da4cdedcba093880a7c1aa327d4ef88bbc
tripledbt1 <151690697+tripledbt1@users.noreply.github.com>
2025-10-28 02:27:54 +0000

    test.py.10.27.4

--- My_Work/test.py
+++ My_Work/test.py
@@ -2,4 +2,4 @@ import numpy as np, seaborn as sns, matplotlib.pyplot as plt
 
 x = np.random.normal(size=500)
 sns.histplot(x, kde=True)
-plt.show()
+plt.show()  # Needs an interpreter to explicitly show the plot when running as a script
